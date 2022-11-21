ef246d7975ec9baeebd8096a445bedd9709ed187
hatemaskar <hatemaskar@gmail.com>
2022-11-21 21:58:07 +0200

    Score fixed

--- score.php
+++ score.php
@@ -6,11 +6,18 @@ $usersquery = 'SELECT * FROM users ORDER BY users.maxscore DESC';
 
 $get_users = mysqli_query($connect,$usersquery);
 
-// while ($playerList = mysqli_fetch_assoc($get_users)){
-// echo '<pre>'; 
-// print_r ($playerList['maxScore']) ; 
-// echo '</pre>';
+while ($playerList = mysqli_fetch_assoc($get_users)){
+
+
+asort($playerList);
+ echo 'Username: ' .$playerList['name']. ' - Score: ' .$playerList['maxScore'];
+
+// foreach($playerList as $name => $age){
+//     echo $name.' - '.$age;
+    echo '<br />';
 // }
+
+}
 
 $connect->close();
 ?>
