718ec120b320a42df9dfae60e9b4d3635d915fea
hatemaskar <hatemaskar@gmail.com>
2022-11-21 10:41:50 +0200

    Add Assets folder
    Add Assets folder

--- score.php
+++ score.php
@@ -2,15 +2,15 @@
 <?php  
 
 $connect = mysqli_connect('127.0.0.1','root','','grinta');
-$usersquery = 'SELECT * FROM users ';
+$usersquery = 'SELECT * FROM users ORDER BY users.maxscore DESC';
 
 $get_users = mysqli_query($connect,$usersquery);
 
-while ($playerList = mysqli_fetch_assoc($get_users)){
-echo '<pre>'; 
-print_r ($playerList['maxScore']) ; 
-echo '</pre>';
-}
+// while ($playerList = mysqli_fetch_assoc($get_users)){
+// echo '<pre>'; 
+// print_r ($playerList['maxScore']) ; 
+// echo '</pre>';
+// }
 
 $connect->close();
 ?>
