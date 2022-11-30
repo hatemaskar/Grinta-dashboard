6e122a1273c15fc2de5b183eb6ae082225c36128
hatemaskar <hatemaskar@gmail.com>
2022-11-30 18:24:41 +0200

    Index version 2 added
    - Assets folder added
    - Responsive design fixed

--- index2.php
+++ index2.php
@@ -1,18 +1,43 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ar">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Grinta</title>
+    <title>Grinta Dashboard V2</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="./style.css" />
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" ></script>
+    <link rel="stylesheet" href="./stylev2.css" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+
 </head>
+
 <body>
-    <div >
-        <h1">Hello</h1>
+    <div class="container">
+        <div class="body-warpper">
+            <div class="header">
+                <div id="header-users"> users</div>
+                <div id="header-questions">questions</div>
+                <div id="header-max-score">score</div>
+            </div>
+            <div class="data-area">
+                <h2> Data Area</h2>
+            </div>
+
+        </div>
+        <div class="side-menu">
+            <div class="logo">
+                <img src="./assets/logo.png" alt="">
+            </div>
+            <div class="menu-items">
+                <li>الرئيسية</li>
+                <li>اللاعبين</li>
+                <li>الأسئلة</li>
+                <li>الإعدادات</li>
+            </div>
+        </div>
     </div>
-    
+
 </body>
+
 </html>
