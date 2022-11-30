1b8eacc5156f663d4c6c1ccd3b6d425414a83091
hatemaskar <hatemaskar@gmail.com>
2022-11-30 14:32:39 +0200

    Table upgrade

--- delete.php
+++ delete.php
@@ -1,15 +1,18 @@
 <?php
 
-if (!isset($_GET["id"])) {
-    $id = $_GET["id"];
 
-    $connect = mysqli_connect('127.0.0.1', 'root', '', 'grinta');
-    $sql = 'DELETE FROM questions WHERE questions.id=$id';
-    $result = $connect->query($sql);
+$id = $_GET["id"];
 
+$connect = mysqli_connect('127.0.0.1', 'root', '', 'grinta');
+$sql = "DELETE FROM questions WHERE id='$id'";
+$result = $connect->query($sql) or die('Error');
+
+if ($result) {
+    header("location: /Grinta-dashboard/postQuestions.php?msg=Question delete successfully!");
+
+} else {
+    echo "Failed: " . mysqli_error($connect);
 }
-
-header("location: /Grinta-dashboard/postQuestions.php");
 exit;
 
 ?>
--- postQuestions.php
+++ postQuestions.php
@@ -7,26 +7,56 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Questions List</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+    <style>
+        table {
+            width: 100%;
+            border: 1px solid #b3adad;
+            border-collapse: collapse;
+            padding: 5px;
+        }
 
+        table th {
+            border: 1px solid #b3adad;
+            padding: 5px;
+            background: #f0f0f0;
+            color: #313030;
+            width: max-content;
+        }
+
+        table td {
+            border: 1px solid #b3adad;
+            text-align: center;
+            padding: 5px;
+            background: #ffffff;
+            color: #313030;
+            width: fit-content;
+        }
+    </style>
 </head>
 
 <body>
     <div class="container">
-        <h2>Questions List</h2>
+        <h2 style="padding-top:100px ;">Questions List</h2>
         <a class="btn btn-primary" href="/Grinta-dashboard/create.php" role="button">Add Question</a>
         <br>
-        <table class="table">
+        <table class="table" style="width:50%" border="5px">
             <thead>
                 <tr>
                     <th>ID</th>
                     <th>Question</th>
                     <th>Correct Answer</th>
+                </tr>
+                <tr>
                     <th>Fake Answer 1</th>
                     <th>Fake Answer 2</th>
                     <th>Fake Answer 3</th>
+                </tr>
+                <tr>
                     <th>Question Type</th>
                     <th>Difficulty</th>
                     <th>category</th>
+                </tr>
+                <tr>
                     <th>Video URL</th>
                 </tr>
             </thead>
@@ -51,22 +81,30 @@
                 while ($row = $result->fetch_assoc()) {
                     echo "
                         <tr>
-                             <td>$row[id]</td>
+                             <td>Q $row[id]</td>
                              <td>$row[question]</td>
                              <td>$row[correctAns]</td>
-                             <td>$row[fakeAns1]</td>
-                             <td>$row[fakeAns2]</td>
-                             <td>$row[fakeAns3]</td>
-                             <td>$row[type]</td>
-                             <td>$row[difficulty]</td>
-                             <td>$row[category]</td>
-                             <td>$row[videoURL]</td>
-                             <td>
-                                <a class='btn btn-primary btn sm' href='/Grinta-dashboard/edit.php?id=$row[id]'>Edit</a>
-                                <a class='btn btn-danger btn sm' href='/Grinta-dashboard/delete.php?id=$row[id]'>Delete</a>
-                                
-                             </td>
+
                         </tr>
+                        <tr>
+                        <td>$row[fakeAns1]</td>
+                        <td>$row[fakeAns2]</td>
+                        <td>$row[fakeAns3]</td>
+                        </tr>
+                        <tr>
+                        <td>Type: $row[type]</td>
+                        <td>Difficulty: $row[difficulty]</td>
+                        <td>Category: $row[category]</td>
+
+                        </tr>
+                        <tr>
+                        <td>Video URL:$row[videoURL]</td>
+                        <td colspan='2'>
+                        <a class='btn btn-primary btn sm' href='/Grinta-dashboard/edit.php?id=$row[id]'>Edit</a>
+                        <a class='btn btn-danger btn sm remove' href='/Grinta-dashboard/delete.php?id=$row[id]'>Delete</a> 
+                        </td>
+                        </tr>
+
                         ";
                 }
                 ?>
@@ -77,6 +115,7 @@
 
 
     </div>
+
 </body>
 
 </html>
